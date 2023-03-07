eb6bba46f78f3926adad9e004a771ba1166cef62
Emin Azadov <emin.azadov.03@gmail.com>
2023-03-07 19:46:49 +0400

    Final fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { globSync } from "glob";
 
 import * as packageJson from "./package.json";
+
+function prepareEntry() {
+  const entry = globSync("src/**/index.ts");
+
+  return entry.reduce((acc, modulePath) => {
+    const updatedModulePath = modulePath.replace("src\\", "").replace(".ts", "");
+
+    return {
+      ...acc,
+      [updatedModulePath]: modulePath,
+    };
+  }, {});
+}
+
+console.log(prepareEntry());
 
 // https://vitejs.dev/config/s
 export default defineConfig({
@@ -13,15 +28,11 @@ export default defineConfig({
     }),
     dts({
       include: ["src/"],
-      rollupTypes: true,
     }),
   ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "./src/index.ts"),
-      // formats: ["es", "cjs"],
-      name: "toyda-react-shared-components",
-      // fileName: (format, entryName) => `${entryName}.${format}.js`,
+      entry: prepareEntry(),
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
